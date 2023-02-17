d316b37203e03fb7cf881351d80c933bdba87b34
aydawka <aydanarxoz_5@mail.ru>
2023-02-17 14:39:17 -0800

    fix: 🐛 props in list component

--- src/components/list/list.stories.js
+++ src/components/list/list.stories.js
@@ -1,105 +1,86 @@
+import { html } from "code-tag";
 import { NButton, NList, NListItem, NSpace, NTag, NThing } from "naive-ui";
 
 export default {
   title: "Components/List",
   component: NList,
-  NSpace,
-  NTag,
-  NButton,
-  NListItem,
-  NThing,
   argTypes: {
-    size: {
-      control: { type: "select" },
-      options: ["medium"],
-    },
+    bordered: { control: { type: "boolean" } },
+    clickable: { control: { type: "boolean" } },
+    hoverable: { control: { type: "boolean" } },
   },
 };
 
 const BasicTemplate = (args) => ({
-  components: { NList },
+  components: { NList, NThing, NListItem, NButton },
   setup() {
     return { args };
   },
-  template:
-    "<n-list>\n" +
-    "    <template #header>\n" +
-    "      hhh\n" +
-    "    </template>\n" +
-    "    <template #footer>\n" +
-    "      fff\n" +
-    "    </template>\n" +
-    "    <n-list-item>\n" +
-    "      <template #prefix>\n" +
-    "        <n-button>Prefix</n-button>\n" +
-    "      </template>\n" +
-    "      <template #suffix>\n" +
-    "        <n-button>Suffix</n-button>\n" +
-    "      </template>\n" +
-    '      <n-thing title="Thing" title-extra="extra" description="description">\n' +
-    "        Biu<br>\n" +
-    "        Biu<br>\n" +
-    "        Biu<br>\n" +
-    "      </n-thing>\n" +
-    "    </n-list-item>\n" +
-    "    <n-list-item>\n" +
-    '      <n-thing title="Thing" title-extra="extra" description="description" />\n' +
-    "    </n-list-item>\n" +
-    "  </n-list>",
+  template: html`<n-list v-bind="args">
+    <template #header> hhh </template>
+    <template #footer> fff </template>
+    <n-list-item>
+      <template #prefix>
+        <n-button>Prefix</n-button>
+      </template>
+      <template #suffix>
+        <n-button>Suffix</n-button>
+      </template>
+      <n-thing title="Thing" title-extra="extra" description="description">
+        Biu<br />
+        Biu<br />
+        Biu<br />
+      </n-thing>
+    </n-list-item>
+    <n-list-item>
+      <n-thing title="Thing" title-extra="extra" description="description" />
+    </n-list-item>
+  </n-list>`,
 });
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = { title: "Basic" };
+Basic.args = {};
 
 const HoverableTemplate = (args) => ({
   components: { NList, NSpace, NTag, NButton, NListItem, NThing },
   setup() {
     return { args };
   },
-  template:
-    "<n-list hoverable clickable>\n" +
-    "    <n-list-item>\n" +
-    '      <n-thing title="Better Late Than Never" content-style="margin-top: 10px;">\n' +
-    "        <template #description>\n" +
-    '          <n-space size="small" style="margin-top: 4px">\n' +
-    '            <n-tag :bordered="false" type="info" size="small">\n' +
-    "              Tag A\n" +
-    "            </n-tag>\n" +
-    '            <n-tag :bordered="false" type="info" size="small">\n' +
-    "              Tag B\n" +
-    "            </n-tag>\n" +
-    "          </n-space>\n" +
-    "        </template>\n" +
-    "        Lorem ipsum dolor sit amet,<br>\n" +
-    "        consectetur adipiscing elit,<br>\n" +
-    "        sed do eiusmod tempor incididunt<br>\n" +
-    "        ut labore et dolore magna aliqua.<br>\n" +
-    "        Ut enim ad minim veniam,<br>\n" +
-    "        quis nostrud exercitation ullamco\n" +
-    "      </n-thing>\n" +
-    "    </n-list-item>\n" +
-    "    <n-list-item>\n" +
-    '      <n-thing title="Lorem Ipsum" content-style="margin-top: 10px;">\n' +
-    "        <template #description>\n" +
-    '          <n-space size="small" style="margin-top: 4px">\n' +
-    '            <n-tag :bordered="false" type="info" size="small">\n' +
-    "              Tag C\n" +
-    "            </n-tag>\n" +
-    '            <n-tag :bordered="false" type="info" size="small">\n' +
-    "              Tag D\n" +
-    "            </n-tag>\n" +
-    "          </n-space>\n" +
-    "        </template>\n" +
-    "        Lorem ipsum dolor sit amet,<br>\n" +
-    "        consectetur adipiscing elit,<br>\n" +
-    "        sed do eiusmod tempor incididunt<br>\n" +
-    "        ut labore et dolore magna aliqua.<br>\n" +
-    "        Ut enim ad minim veniam,<br>\n" +
-    "        quis nostrud exercitation ullamco\n" +
-    "      </n-thing>\n" +
-    "    </n-list-item>\n" +
-    "  </n-list>",
+  template: html` <n-list hoverable clickable v-bind="args">
+    <n-list-item>
+      <n-thing title="Better Late Than Never" content-style="margin-top: 10px;">
+        <template #description>
+          <n-space size="small" style="margin-top: 4px">
+            <n-tag :bordered="false" type="info" size="small"> Tag A </n-tag>
+            <n-tag :bordered="false" type="info" size="small"> Tag B </n-tag>
+          </n-space>
+        </template>
+        Lorem ipsum dolor sit amet,<br />
+        consectetur adipiscing elit,<br />
+        sed do eiusmod tempor incididunt<br />
+        ut labore et dolore magna aliqua.<br />
+        Ut enim ad minim veniam,<br />
+        quis nostrud exercitation ullamco
+      </n-thing>
+    </n-list-item>
+    <n-list-item>
+      <n-thing title="Lorem Ipsum" content-style="margin-top: 10px;">
+        <template #description>
+          <n-space size="small" style="margin-top: 4px">
+            <n-tag :bordered="false" type="info" size="small"> Tag C </n-tag>
+            <n-tag :bordered="false" type="info" size="small"> Tag D </n-tag>
+          </n-space>
+        </template>
+        Lorem ipsum dolor sit amet,<br />
+        consectetur adipiscing elit,<br />
+        sed do eiusmod tempor incididunt<br />
+        ut labore et dolore magna aliqua.<br />
+        Ut enim ad minim veniam,<br />
+        quis nostrud exercitation ullamco
+      </n-thing>
+    </n-list-item>
+  </n-list>`,
 });
 
 export const Hoverable = HoverableTemplate.bind({});
-Hoverable.args = { size: "medium" };
+Hoverable.args = {};
