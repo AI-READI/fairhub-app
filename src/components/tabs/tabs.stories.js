1d4b16181cf67001ef2719a830ea9071fabda0e6
aydawka <aydanarxoz_5@mail.ru>
2023-02-19 10:47:39 -0800

    feat: ✨ adding props in tabs component

--- src/components/tabs/tabs.stories.js
+++ src/components/tabs/tabs.stories.js
@@ -1,11 +1,25 @@
+import { html } from "code-tag";
 import { NButton, NCard, NForm, NFormItemRow, NInput, NTabPane, NTabs } from "naive-ui";
+
 export default {
   title: "Components/Tabs",
   component: NTabs,
   argTypes: {
     size: {
       control: { type: "select" },
-      options: ["medium"],
+      options: ["small", "medium", "large"],
+    },
+    type: {
+      control: { type: "select" },
+      options: ["bar", "line", "card", "segment"],
+    },
+    closable: {
+      control: { type: "boolean" },
+    },
+
+    trigger: {
+      control: { type: "select" },
+      options: ["click", "hover"],
     },
   },
 };
@@ -15,76 +29,65 @@ const BasicTemplate = (args) => ({
   setup() {
     return { args };
   },
-  template:
-    '  <n-card title="Song of" style="margin-bottom: 16px">\n' +
-    '    <n-tabs default-value="oasis" justify-content="space-evenly" type="line">\n' +
-    '      <n-tab-pane name="oasis" tab="Oasis">\n' +
-    "        Wonderwall\n" +
-    "      </n-tab-pane>\n" +
-    '      <n-tab-pane name="the beatles" tab="the Beatles">\n' +
-    "        Hey Jude\n" +
-    "      </n-tab-pane>\n" +
-    '      <n-tab-pane name="jay chou" tab="Jay Chou">\n' +
-    "        Qilixiang\n" +
-    "      </n-tab-pane>\n" +
-    "    </n-tabs>\n" +
-    "  </n-card>\n" +
-    '  <n-card title="Too Simple">\n' +
-    '    <n-tabs default-value="signin" size="large" justify-content="space-evenly">\n' +
-    '      <n-tab-pane name="signin" tab="Sign in">\n' +
-    "        <n-form>\n" +
-    '          <n-form-item-row label="Username">\n' +
-    "            <n-input />\n" +
-    "          </n-form-item-row>\n" +
-    '          <n-form-item-row label="Password">\n' +
-    "            <n-input />\n" +
-    "          </n-form-item-row>\n" +
-    "        </n-form>\n" +
-    '        <n-button type="primary" block secondary strong>\n' +
-    "          Sign In\n" +
-    "        </n-button>\n" +
-    "      </n-tab-pane>\n" +
-    '      <n-tab-pane name="signup" tab="Sign up">\n' +
-    "        <n-form>\n" +
-    '          <n-form-item-row label="Username">\n' +
-    "            <n-input />\n" +
-    "          </n-form-item-row>\n" +
-    '          <n-form-item-row label="Password">\n' +
-    "            <n-input />\n" +
-    "          </n-form-item-row>\n" +
-    '          <n-form-item-row label="Reenter Password">\n' +
-    "            <n-input />\n" +
-    "          </n-form-item-row>\n" +
-    "        </n-form>\n" +
-    '        <n-button type="primary" block secondary strong>\n' +
-    "          Sign up\n" +
-    "        </n-button>\n" +
-    "      </n-tab-pane>\n" +
-    "    </n-tabs>\n" +
-    "  </n-card>",
+  template: html` <n-card title="Song of">
+      <n-tabs animated v-bind="args">
+        <n-tab-pane name="oasis" tab="Oasis"> Wonderwall </n-tab-pane>
+        <n-tab-pane name="the beatles" tab="the Beatles"> Hey Jude </n-tab-pane>
+        <n-tab-pane name="jay chou" tab="Jay Chou"> Qilixiang </n-tab-pane>
+      </n-tabs>
+    </n-card>
+    <n-card>
+      <n-tabs
+        class="card-tabs"
+        default-value="signin"
+        size="large"
+        animated
+        style="margin: 0 -4px"
+        pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+      >
+        <n-tab-pane name="signin" tab="Sign in" v-bind="Args">
+          <n-form>
+            <n-form-item-row label="Username">
+              <n-input />
+            </n-form-item-row>
+            <n-form-item-row label="Password">
+              <n-input />
+            </n-form-item-row>
+          </n-form>
+          <n-button type="primary" block secondary strong> Sign In </n-button>
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="Sign up">
+          <n-form>
+            <n-form-item-row label="Username">
+              <n-input />
+            </n-form-item-row>
+            <n-form-item-row label="Password">
+              <n-input />
+            </n-form-item-row>
+            <n-form-item-row label="Reenter Password">
+              <n-input />
+            </n-form-item-row>
+          </n-form>
+          <n-button type="primary" block secondary strong> Sign up </n-button>
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>`,
 });
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = { title: "Basic" };
+Basic.args = {};
 
 const SegmentTemplate = (args) => ({
   components: { NTabs, NTabPane },
   setup() {
     return { args };
   },
-  template:
-    '  <n-tabs type="segment">\n' +
-    '    <n-tab-pane name="oasis" tab="Oasis">\n' +
-    "      Wonderwall\n" +
-    "    </n-tab-pane>\n" +
-    '    <n-tab-pane name="the beatles" tab="the Beatles">\n' +
-    "      Hey Jude\n" +
-    "    </n-tab-pane>\n" +
-    '    <n-tab-pane name="jay chou" tab="Jay Chou">\n' +
-    "      Qilixiang\n" +
-    "    </n-tab-pane>\n" +
-    "  </n-tabs>",
+  template: html` <n-tabs type="segment" v-bind="args">
+    <n-tab-pane name="oasis" tab="Oasis" v-bind="args"> Wonderwall </n-tab-pane>
+    <n-tab-pane name="the beatles" tab="the Beatles"> Hey Jude </n-tab-pane>
+    <n-tab-pane name="jay chou" tab="Jay Chou"> Qilixiang </n-tab-pane>
+  </n-tabs>`,
 });
 
 export const Segment = SegmentTemplate.bind({});
-Segment.args = { size: "medium" };
+Segment.args = {};
