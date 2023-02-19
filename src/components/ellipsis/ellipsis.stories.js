4b8fbc40f0e223c3c20c0666c64ba02df15da12b
aydawka <aydanarxoz_5@mail.ru>
2023-02-19 12:15:44 -0800

    feat: ✨ adding props in ellipsis component

--- src/components/ellipsis/ellipsis.stories.js
+++ src/components/ellipsis/ellipsis.stories.js
@@ -1,13 +1,10 @@
+import { html } from "code-tag";
 import { NEllipsis } from "naive-ui";
+
 export default {
   title: "Components/Ellipsis",
   component: NEllipsis,
-  argTypes: {
-    size: {
-      control: { type: "select" },
-      options: ["medium"],
-    },
-  },
+  argTypes: {},
 };
 
 const BasicTemplate = (args) => ({
@@ -15,54 +12,49 @@ const BasicTemplate = (args) => ({
   setup() {
     return { args };
   },
-  template:
-    '<n-ellipsis style="max-width: 240px">\n' +
-    "    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium\n" +
-    "    doloremque laudantium, totam rem aperiam\n" +
-    "  </n-ellipsis>",
+  template: html` <n-ellipsis style="max-width: 240px" v-bind="args">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+  </n-ellipsis>`,
 });
 
 export const Basic = BasicTemplate.bind({});
-Basic.args = { title: "Ellipsis" };
+Basic.args = {};
 
 const ExpandTemplate = (args) => ({
   components: { NEllipsis },
   setup() {
     return { args };
   },
-  template:
-    '  <n-ellipsis expand-trigger="click" line-clamp="2" :tooltip="false">\n' +
-    "    Lorem ipsum dolor sit amet,<br>\n" +
-    "    consectetur adipiscing elit,<br>\n" +
-    "    sed do eiusmod tempor incididunt<br>\n" +
-    "    ut labore et dolore magna aliqua.<br>\n" +
-    "    Ut enim ad minim veniam,<br>\n" +
-    "    quis nostrud exercitation ullamco\n" +
-    "  </n-ellipsis>",
+  template: html` <n-ellipsis expand-trigger="click" line-clamp="2" :tooltip="false" v-bind="args">
+    Lorem ipsum dolor sit amet,<br />
+    consectetur adipiscing elit,<br />
+    sed do eiusmod tempor incididunt<br />
+    ut labore et dolore magna aliqua.<br />
+    Ut enim ad minim veniam,<br />
+    quis nostrud exercitation ullamco
+  </n-ellipsis>`,
 });
 
 export const Expand = ExpandTemplate.bind({});
-Expand.args = { size: "medium" };
+Expand.args = {};
 
 const TooltipTemplate = (args) => ({
   components: { NEllipsis },
   setup() {
     return { args };
   },
-  template:
-    ' <n-ellipsis style="max-width: 240px">\n' +
-    "    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium\n" +
-    "    doloremque laudantium, totam rem aperiam\n" +
-    "    <template #tooltip>\n" +
-    '      <div style="text-align: center">\n' +
-    "        <i>Lorem Ipsum</i><br>\n" +
-    "        Sed ut perspiciatis unde omnis<br>\n" +
-    "        iste natus error sit voluptatem accusantium doloremque laudantium,<br>\n" +
-    "        totam rem aperiam\n" +
-    "      </div>\n" +
-    "    </template>\n" +
-    "  </n-ellipsis>",
+  template: html` <n-ellipsis style="max-width: 240px" v-bind="args">
+    Sed ut perspiciatis unde omnis iste
+    <template #tooltip>
+      <div style="text-align: center">
+        <i>Lorem Ipsum</i><br />
+        Sed ut perspiciatis unde omnis<br />
+        iste natus error sit <br />
+        totam rem aperiam
+      </div>
+    </template>
+  </n-ellipsis>`,
 });
 
 export const Tooltip = TooltipTemplate.bind({});
-Tooltip.args = { size: "medium" };
+Tooltip.args = {};
