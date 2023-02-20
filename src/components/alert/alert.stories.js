a2b7c8848a834ed7c4999b95f7ebfee7674ad3da
aydawka <aydanarxoz_5@mail.ru>
2023-02-19 19:06:01 -0800

    fix: 🐛 modified props in alert component

--- src/components/alert/alert.stories.js
+++ src/components/alert/alert.stories.js
@@ -1,73 +1,42 @@
-import { NAlert, NSpace } from "naive-ui";
-
+import { html } from "code-tag";
+import { NAlert, NIcon, NSpace } from "naive-ui";
 export default {
   title: "Components/Alert",
   component: NAlert,
   argTypes: {
-    size: {
+    type: {
       control: { type: "select" },
-      options: ["medium"],
+      options: ["primary", "info", "success", "warning", "error"],
     },
+    closable: { control: { type: "boolean" } },
+    bordered: { control: { type: "boolean" } },
   },
 };
 
 const TitleTemplate = (args) => ({
-  components: { NAlert, NSpace },
+  components: { NAlert, NSpace, NIcon },
   setup() {
     return { args };
   },
-  template:
-    ' <n-space vertical :size="12">\n' +
-    '    <n-alert title="Default Text" type="default">\n' +
-    "      <template #icon>\n" +
-    "        <n-icon>\n" +
-    "      </template>\n" +
-    "      Gee it's good to be back home\n" +
-    "    </n-alert>\n" +
-    '    <n-alert title="Info Text" type="info">\n' +
-    "      Gee it's good to be back home\n" +
-    "    </n-alert>\n" +
-    '    <n-alert title="Success Text" type="success">\n' +
-    "      Leave it till tomorrow to unpack my case\n" +
-    "    </n-alert>\n" +
-    '    <n-alert title="Warning Text" type="warning">\n' +
-    "      Honey disconnect the phone\n" +
-    "    </n-alert>\n" +
-    '    <n-alert title="Error Text" type="error">\n' +
-    "      I'm back in the U.S.S.R.\n" +
-    "    </n-alert>\n" +
-    "  </n-space>",
+  template: html` <n-space vertical>
+    <n-alert title="Default Text" type="default" v-bind="args">
+      <template #icon>
+        <n-icon>
+          <ios-airplane />
+        </n-icon>
+      </template>
+      Gee it's good to be back home
+    </n-alert>
+    <n-alert title="Info Text" type="info" v-bind="args"> Gee it's good to be back home </n-alert>
+    <n-alert title="Success Text" type="success" v-bind="args">
+      Leave it till tomorrow to unpack my case
+    </n-alert>
+    <n-alert title="Warning Text" type="warning" v-bind="args">
+      Honey disconnect the phone
+    </n-alert>
+    <n-alert title="Error Text" type="error"> I'm back in the U.S.S.R. </n-alert>
+  </n-space>`,
 });
 
 export const Title = TitleTemplate.bind({});
-Title.args = { title: "Rate" };
-
-const ClosableTemplate = (args) => ({
-  components: { NAlert, NSpace },
-  setup() {
-    return { args };
-  },
-  template:
-    '  <n-alert title="Default Text" type="default" closable>\n' +
-    "    <template #icon>\n" +
-    "      <n-icon>\n" +
-    "      <n-icon>\n" +
-    "    </template>\n" +
-    "    Gee it's good to be back home\n" +
-    "  </n-alert>\n" +
-    '  <n-alert title="Info Text" type="info" closable>\n' +
-    "    Gee it's good to be back home\n" +
-    "  </n-alert>\n" +
-    '  <n-alert title="Success Text" type="success" closable>\n' +
-    "    Leave it till tomorrow to unpack my case\n" +
-    "  </n-alert>\n" +
-    '  <n-alert title="Warning Text" type="warning" closable>\n' +
-    "    Honey disconnect the phone\n" +
-    "  </n-alert>\n" +
-    '  <n-alert title="Error Text" type="error" closable>\n' +
-    "    I'm back in the U.S.S.R.\n" +
-    "  </n-alert>",
-});
-
-export const Closable = ClosableTemplate.bind({});
-Closable.args = { size: "medium" };
+Title.args = {};
