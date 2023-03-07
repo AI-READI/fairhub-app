c1d2c4829bebf0dba2c2a6397c92529928436f17
aydawka <aydanarxoz_5@mail.ru>
2023-03-07 14:54:50 -0800

    feat: ✨ skeleton added to integration page

--- src/views/IntegrationPage.vue
+++ src/views/IntegrationPage.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-import { NButton } from "naive-ui";
+import { NSkeleton } from "naive-ui";
 </script>
 
 <template>
   <h1>Integrations</h1>
-  <n-p
-    >An overview of all your accounts connected to FAIRhub and the actions you can take are shown
-    below.</n-p
-  >
-  <n-button>integrations</n-button>
+  <div>
+    An overview of all your accounts connected to FAIRhub and the actions you can take are shown
+    below.
+  </div>
+  <n-space vertical>
+    <n-skeleton text height="40px" :repeat="7" style="width: 60%" />
+    <n-skeleton text height="40px" style="width: 60%" />
+    <n-skeleton height="40px" width="33%" />
+  </n-space>
 </template>
