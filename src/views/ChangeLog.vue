7f079fbe6810f4dc50f0d544b77ab022d5d948c5
aydawka <aydanarxoz_5@mail.ru>
2023-03-10 11:09:54 -0800

    fix: 🐛 changelog page is fixed

--- src/views/ChangeLog.vue
+++ src/views/ChangeLog.vue
@@ -15,16 +15,31 @@ const markdownToHtml = computed(() => {
 </script>
 
 <template>
-  <n-p> All notable changes for FAIRhub will be documented in this file. </n-p>
-  <n-alert title="Info Text" type="info">
-    GThe format is based on
-    <a href="http://keepachangelog.com/en/1.0.0/" target="_blank" rel="noopener noreferrer">
-      Keep a Changelog</a
-    >
-    and this project adheres to
-    <a href="http://semver.org/spec/v2.0.0.html" target="_blank" rel="noopener noreferrer">
-      Semantic Versioning
-    </a>
-  </n-alert>
-  <div v-html="markdownToHtml"></div>
+  <div class="changelog prose">
+    <h1>Changelog</h1>
+    <n-p> All notable changes for FAIRhub will be documented in this file. </n-p>
+    <div class="alert">
+      <n-alert title="Info Text" type="info">
+        The format is based on
+        <a href="http://keepachangelog.com/en/1.0.0/" target="_blank" rel="noopener noreferrer">
+          Keep a Changelog</a
+        >
+        and this project adheres to
+        <a href="http://semver.org/spec/v2.0.0.html" target="_blank" rel="noopener noreferrer">
+          Semantic Versioning
+        </a>
+      </n-alert>
+    </div>
+
+    <div v-html="markdownToHtml" class="prose"></div>
+  </div>
 </template>
+<style>
+.changelog {
+  padding-left: 1rem;
+}
+
+.alert {
+  padding-bottom: 3.5rem;
+}
+</style>
