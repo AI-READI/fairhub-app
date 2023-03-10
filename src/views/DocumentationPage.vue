53e06b4c53338c28069a4218c866b45d23724444
aydawka <aydanarxoz_5@mail.ru>
2023-03-10 10:16:06 -0800

    fix: 🐛 docs page is fixed

--- src/views/DocumentationPage.vue
+++ src/views/DocumentationPage.vue
@@ -9,16 +9,20 @@ import Docs from "../assets/animations/documents_page.json";
 
 <template>
   <div class="docs">
-    <h1>Documentation</h1>
-    <n-p>
-      We have created a documentation page for each feature in FAIRhub. You can find the
-      documentation site by clicking on the button below:
-    </n-p>
-    <n-button>
-      <n-a href="www.docs.fairhub.io" target="_blank">View the Documentation</n-a>
-    </n-button>
+    <div class="row">
+      <h1 class="docs-header">Documentation</h1>
+      <n-p>
+        We have created a documentation page for each feature in FAIRhub. You can find the
+        documentation site by clicking on the button below:
+      </n-p>
+      <div><Vue3Lottie :animationData="Docs" :height="400" :width="400" /></div>
+    </div>
+    <div class="docs-button">
+      <n-button>
+        <n-a href="www.docs.fairhub.io" target="_blank">View the Documentation</n-a>
+      </n-button>
+    </div>
   </div>
-  <div><Vue3Lottie :animationData="Docs" :height="400" :width="400" /></div>
 </template>
 
 <style>
@@ -26,5 +30,18 @@ import Docs from "../assets/animations/documents_page.json";
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.row {
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.docs-header {
+  padding-top: 2rem;
+}
+
+.docs-button {
+  padding-top: 2rem;
 }
 </style>
