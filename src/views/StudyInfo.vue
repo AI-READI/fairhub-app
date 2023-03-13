800fe777c408eb06335e42e58ab86b6943679a06
aydawka <aydanarxoz_5@mail.ru>
2023-03-13 11:23:46 -0700

    fix: 🐛 study info descriptions fixed

--- src/views/StudyInfo.vue
+++ src/views/StudyInfo.vue
@@ -1,128 +1,134 @@
-<script lang="ts">
-import type { FormInst, FormItemInst, FormItemRule, FormRules } from "naive-ui";
-import { NButton, NDynamicTags, NForm, NFormItem, NInput, NTag } from "naive-ui";
-import { defineComponent, h, ref } from "vue";
+<script setup lang="ts">
+import type { FormInst, FormItemRule, FormRules } from "naive-ui";
+import { NButton, NForm, NFormItem, NInput, NSelect, NSpace, useMessage } from "naive-ui";
+import { onBeforeMount, ref } from "vue";
+import { useRouter } from "vue-router";
 
-interface ModelType {
-  title: string | null;
-  description: string | null;
-  keywords: string | null;
-}
+import { useAuthStore } from "@/stores/auth";
 
-export default defineComponent({
-  components: { NButton, NDynamicTags, NForm, NFormItem, NInput },
-  setup() {
-    const tagsRef = ref(["FAIRhub", "biomedical"]);
-    const formRef = ref<FormInst | null>(null);
-    const rPasswordFormItemRef = ref<FormItemInst | null>(null);
-    // const message = useMessage()
-    const modelRef = ref<ModelType>({
-      title: "",
-      description: "",
-      keywords: "",
-    });
-    const rules: FormRules = {
-      title: [
-        {
-          required: true,
-          trigger: ["input", "blur"],
-          validator(rule: FormItemRule, value: string) {
-            if (!value) {
-              return new Error("keywords is required");
-            }
-            return true;
-          },
-        },
-      ],
-      description: [
-        {
-          required: true,
-          trigger: ["input", "blur"],
-          validator(rule: FormItemRule, value: string) {
-            if (!value) {
-              return new Error("keywords is required");
-            }
-            return true;
-          },
-        },
-      ],
-    };
-    return {
-      formRef,
-      model: modelRef,
-      renderTag: (tag: string, index: number) => {
-        return h(
-          NTag,
-          {
-            closable: true,
-            disabled: index > 3,
-            onClose: () => {
-              tagsRef.value.splice(index, 1);
-            },
-            type: index < 3 ? "success" : "error",
-          },
-          {
-            default: () => tag,
-          }
-        );
-      },
-      rPasswordFormItemRef,
-      rules,
-      tags: tagsRef,
-      // handleValidateButtonClick (e: MouseEvent) {
-      //   e.preventDefault()
-      //   formRef.value?.validate(
-      //     (errors: Array<FormValidationError> | undefined) => {
-      //       if (!errors) {
-      //         message.success('Valid')
-      //       } else {
-      //         console.log(errors)
-      //         message.error('Invalid')
-      //       }
-      //     }
-      //   )
-      // }
-    };
-  },
+const router = useRouter();
+const authStore = useAuthStore();
+const { error } = useMessage();
+
+onBeforeMount(() => {
+  if (!authStore.isAuthenticated) {
+    error("You are not logged in.");
+    router.push({ name: "home" });
+  }
 });
+
+const formRef = ref<FormInst | null>(null);
+
+const study = ref({
+  title: null,
+  description: null,
+  keywords: null,
+});
+
+const generalOptions = [
+  "Artifical Intelligence",
+  "Dataset",
+  "Diabetes",
+  "Ethics",
+  "Health",
+  "Machine Learning",
+].map((v) => ({
+  label: v,
+  value: v,
+}));
+
+const rules: FormRules = {
+  description: [
+    {
+      message: "Please input a study description",
+      required: true,
+      trigger: ["blur", "input"],
+    },
+  ],
+  inputValue: [
+    {
+      message: "Please add a study title",
+      required: true,
+      trigger: ["blur", "input"],
+    },
+  ],
+  keywords: [
+    {
+      required: true,
+      trigger: ["blur", "change"],
+      validator: (rule: FormItemRule, value) => {
+        if (value !== null && value.length > 0) {
+          return Promise.resolve();
+        }
+        return Promise.reject("Please select at least one keyword");
+      },
+    },
+  ],
+};
+
+const handleValidateButtonClick = (e: MouseEvent) => {
+  e.preventDefault();
+  formRef.value?.validate((errors) => {
+    if (!errors) {
+      console.log("success");
+    } else {
+      console.log("error");
+      console.log(errors);
+    }
+  });
+};
+
+const handleUpdateValue = (value: string[]) => {
+  console.log(value);
+};
 </script>
 
 <template>
-  <main class="flex h-full w-full flex-col space-y-8 pr-8">
-    <h1>Enter information</h1>
-    <n-form ref="formRef" :model="model" :rules="rules">
-      <n-form-item path="title" label="Title">
-        <n-input v-model:value="model.title" @keydown.enter.prevent />
+  <main class="flex h-full w-full flex-col space-y-8">
+    <n-space justify="space-between">
+      <h1>Enter information</h1>
+    </n-space>
+
+    <n-form
+      ref="formRef"
+      :model="study"
+      :rules="rules"
+      size="large"
+      label-placement="top"
+      class="pr-4"
+    >
+      <n-form-item :span="12" label="Title" path="title">
+        <n-input v-model:value="study.title" placeholder="Add a study title" />
       </n-form-item>
-      <n-form-item path="description" label="Description">
+
+      <n-form-item :span="12" label="Description" path="description">
         <n-input
-          v-model:value="model.description"
+          v-model:value="study.description"
           placeholder="Add a study description"
           type="textarea"
           :autosize="{
             minRows: 3,
             maxRows: 5,
           }"
-          @keydown.enter.prevent
         />
       </n-form-item>
-      <n-form-item path="keywords" label="Keywords">
-        <n-input v-model:value="model.keywords" @keydown.enter.prevent />
+
+      <n-form-item :span="12" label="Keywords" path="keywords">
+        <n-select
+          v-model:value="study.keywords"
+          placeholder="Salutogenesis"
+          multiple
+          tag
+          filterable
+          clearable
+          :options="generalOptions"
+          @update:value="handleUpdateValue"
+        />
       </n-form-item>
-      <n-dynamic-tags v-model:value="tags" :render-tag="renderTag" />
-      <n-row :gutter="[0, 24]">
-        <n-col :span="24">
-          <div style="display: flex; justify-content: flex-end">
-            <n-button
-              :disabled="model.keywords === null && model.title === null"
-              round
-              type="primary"
-            >
-              Submit
-            </n-button>
-          </div>
-        </n-col>
-      </n-row>
+
+      <div class="flex justify-start">
+        <n-button type="primary" size="large" @click="handleValidateButtonClick"> Update </n-button>
+      </div>
     </n-form>
   </main>
 </template>
