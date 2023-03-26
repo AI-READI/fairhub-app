66177eac2e56ee324865c16810dfefc3098b40a1
aydawka <aydan.gasimova2@gmail.com>
2023-03-26 12:46:52 -0700

    feat: ✨ added view profile upload ad password

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,18 +1,12 @@
-import type { UploadFileInfo } from "naive-ui";
 import { ref } from "vue";
 
 export const userProfile = ref({
+  username: "admin",
   email: "aydan.gasimova2@gmail.com",
-  fileListRef: <UploadFileInfo[]>[
-    {
-      id: "1",
-      name: `https://api.dicebear.com/5.x/shapes/svg?seed=${Math.random()}`,
-      status: "finished",
-      type: ".png",
-    },
-  ],
   fullname: "Aydan Gasimova",
+  image: ` https://api.dicebear.com/5.x/shapes/svg?seed=${Math.random()}`,
   institution: "CALMI2",
   location: "San Diego, CA",
+  password: "admin",
   timezone: "(GMT-11:00) Midway Island",
 });
