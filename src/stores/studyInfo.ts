9908b68080ca84576531718b2a8a40d6d77d2707
aydawka <aydan.gasimova2@gmail.com>
2023-04-14 17:47:37 -0700

    fix: 🐛 modified publish study metadata page

---
+++ src/stores/studyInfo.ts
@@ -0,0 +1,8 @@
+import { ref } from "vue";
+
+export const study = ref({
+  title: "AI-READI",
+  description:
+    "The AI-READI project seeks to create and share a flagship ethically-sourced dataset of type 2 diabetes.",
+  keywords: ["Machine learning", "FAIR"],
+});
--- src/views/publish/PublishStudyMetadata.vue
+++ src/views/publish/PublishStudyMetadata.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-import { NButton, NCard, useMessage } from "naive-ui";
+import type { FormInst, FormItemRule, FormRules } from "naive-ui";
+import { NButton, NFormItem, NInput, NSelect, NSpace, useMessage } from "naive-ui";
 import { onBeforeMount, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import { useAuthStore } from "@/stores/auth";
 import { currentRef } from "@/stores/publish/currentStep";
-import { formValue } from "@/stores/publish/datasetMetadata";
+import { study } from "@/stores/studyInfo";
 
 const router = useRouter();
 const authStore = useAuthStore();
 const { error } = useMessage();
+
+const route = useRoute();
+const routeParams = {
+  versionId: route.params.versionId.toString(),
+};
 
 onBeforeMount(() => {
   if (!authStore.isAuthenticated) {
@@ -18,18 +24,64 @@ onBeforeMount(() => {
   }
 });
 
-const route = useRoute();
+const formRef = ref<FormInst | null>(null);
 
-const routeParams = {
-  versionId: route.params.versionId.toString(),
+const generalOptions = [
+  "Artifical Intelligence",
+  "Dataset",
+  "Diabetes",
+  "Ethics",
+  "Health",
+  "Machine Learning",
+].map((v) => ({
+  label: v,
+  value: v,
+}));
+
+const rules: FormRules = {
+  description: [
+    {
+      message: "Please input a study description",
+      required: true,
+      trigger: ["blur", "input"],
+    },
+  ],
+  inputValue: [
+    {
+      message: "Please add a study title",
+      required: true,
+      trigger: ["blur", "input"],
+    },
+  ],
+  keywords: [
+    {
+      required: true,
+      trigger: ["blur", "change"],
+      validator: (rule: FormItemRule, value) => {
+        if (value !== null && value.length > 0) {
+          return Promise.resolve();
+        }
+        return Promise.reject("Please select at least one keyword");
+      },
+    },
+  ],
 };
 
-const checkingForPreviousVersions = ref(true);
+const handleValidateButtonClick = (e: MouseEvent) => {
+  e.preventDefault();
+  formRef.value?.validate((errors) => {
+    if (!errors) {
+      console.log("success");
+    } else {
+      console.log("error");
+      console.log(errors);
+    }
+  });
+};
 
-setTimeout(() => {
-  checkingForPreviousVersions.value = false;
-}, 3000);
-
+const handleUpdateValue = (value: string[]) => {
+  console.log(value);
+};
 function handleBackButton() {
   currentRef.value--;
   router.push({
@@ -49,26 +101,49 @@ function handleNextButton() {
 
 <template>
   <main class="flex h-full w-full flex-col space-y-8 pr-8">
-    <h1>Review study information</h1>
-    <n-card class="!mt-4">
-      <div class="flex flex-col" :key="formValue.title">
-        <dd class="font-bold">Title:</dd>
-        <dl>{{ formValue.title }}</dl>
-      </div>
-      <div class="flex flex-col" :key="formValue.description">
-        <dd class="font-bold">Description:</dd>
-        <dl>{{ formValue.description }}</dl>
-      </div>
-      <div class="font-bold">Keyword:</div>
-      <div class="flex flex-col" :key="index" v-for="(item, index) in formValue.keywords">
-        <div>{{ item }}</div>
-      </div>
-      <div class="flex flex-col" :key="formValue.primaryLanguage">
-        <dd class="font-bold">Primary language:</dd>
-        <dl>{{ formValue.primaryLanguage }}</dl>
-      </div>
-    </n-card>
+    <n-space justify="space-between">
+      <h1>Review study information</h1>
+    </n-space>
+    <n-form
+      ref="formRef"
+      :model="study"
+      :rules="rules"
+      size="large"
+      label-placement="top"
+      class="pr-4"
+    >
+      <n-form-item :span="12" label="Title" path="title">
+        <n-input v-model:value="study.title" placeholder="Add a study title" />
+      </n-form-item>
+      <n-form-item :span="12" label="Description" path="description">
+        <n-input
+          v-model:value="study.description"
+          placeholder="Add a study description"
+          type="textarea"
+          :autosize="{
+            minRows: 3,
+            maxRows: 5,
+          }"
+        />
+      </n-form-item>
 
+      <n-form-item :span="12" label="Keywords" path="keywords">
+        <n-select
+          v-model:value="study.keywords"
+          placeholder="Salutogenesis"
+          multiple
+          tag
+          filterable
+          clearable
+          :options="generalOptions"
+          @update:value="handleUpdateValue"
+        />
+      </n-form-item>
+
+      <div class="flex justify-start">
+        <n-button type="primary" size="large" @click="handleValidateButtonClick"> Update </n-button>
+      </div>
+    </n-form>
     <div class="back-next-buttons">
       <n-button type="primary" size="large" @click="handleBackButton">Back</n-button>
       <n-button type="primary" size="large" @click="handleNextButton">Next</n-button>
