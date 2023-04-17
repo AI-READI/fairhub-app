cd6ab5632ce6245731bc0e60b2d6f12f048c7520
aydawka <aydan.gasimova2@gmail.com>
2023-04-17 12:48:36 -0700

    fix: 🐛 modified additional info page

--- src/views/publish/PublishAdditionalInfo.vue
+++ src/views/publish/PublishAdditionalInfo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { NButton } from "naive-ui";
+import { NButton, NCard, NInput } from "naive-ui";
 import { useRoute } from "vue-router";
 
 import router from "@/router";
@@ -29,10 +29,37 @@ function handleNextButton() {
 
 <template>
   <main class="flex h-full w-full flex-col space-y-8 pr-8">
-    <div class="person-detail">
-      <h1>Additional information</h1>
+    <h1>Additional information</h1>
+    <div class="additional-info">
+      <n-card title="Edit/Add funding">
+        <n-input placeholder="Edit funding" />
+        <div class="save-additional-info">
+          <n-button type="success">Save</n-button>
+        </div>
+      </n-card>
+
+      <n-card title="Update dataset version">
+        <n-input placeholder="Update dataset version" />
+        <div class="save-additional-info">
+          <n-button type="success">Save</n-button>
+        </div>
+      </n-card>
+      <n-card title="Update dataset publication date">
+        <n-input placeholder="Update dataset publication date" />
+        <div class="save-additional-info">
+          <n-button type="success">Save</n-button>
+        </div>
+      </n-card>
+      <n-card title="Select review license and access type">
+        <div class="review-and-access">
+          <n-input placeholder="Select review license" />
+          <n-input placeholder="Select access type" />
+        </div>
+        <div class="save-additional-info">
+          <n-button type="success">Save</n-button>
+        </div>
+      </n-card>
     </div>
-    <div></div>
     <div class="back-next-buttons">
       <n-button type="primary" size="large" @click="handleBackButton">Back</n-button>
       <n-button type="primary" size="large" @click="handleNextButton">Next</n-button>
@@ -40,13 +67,20 @@ function handleNextButton() {
   </main>
 </template>
 <style>
-.person-detail {
+.additional-info {
   display: flex;
   flex-direction: column;
+  gap: 1rem;
+}
+.review-and-access {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
-.next-button {
+.save-additional-info {
+  margin-top: 1rem;
   display: flex;
-  justify-content: flex-end !important;
+  justify-content: flex-end;
 }
 </style>
