017df8d0acc63f04ade7350fa15dee97e3127b31
aydawka <aydan.gasimova2@gmail.com>
2023-05-23 10:38:50 -0700

    fix: 🐛 modified async file to then promise

--- src/stores/studies.ts
+++ src/stores/studies.ts
@@ -1,11 +1,15 @@
+import type { Ref } from "vue";
 import { ref } from "vue";
 
+import type { Study } from "@/stores/publish/study-interfaces";
 import { fetchStudies } from "@/stores/services/service";
 
-export const studies = ref(await fetchStudies());
-export const getStudy = (id: number) => {
+export const studies: Ref<Study[]> = ref([]);
+fetchStudies().then();
+
+export const getStudy = (studyId: number) => {
   // Check if study exists
-  const study = studies.value.find((study) => study.id === id);
+  const study = studies.value.find((study) => study.id === studyId);
 
   if (!study) {
     throw new Error("Study not found");
