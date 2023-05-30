5505ae6e989f3db788068a16b14759a647db17c1
aydawka <aydan.gasimova2@gmail.com>
2023-05-30 13:10:08 -0700

    fix: 🐛 restructured publish directory

--- src/stores/publish/version.ts
+++ src/stores/publish/version.ts
@@ -1,5 +1,5 @@
 import type { Ref } from "vue";
 import { ref } from "vue";
 
-export const versionChecked = ref(false);
+export const datasetExist = ref(false);
 export const previousVersion: Ref<null | string> = ref(null);
