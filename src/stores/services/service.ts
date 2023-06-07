175a35effc0f3e4d295adea32eec92dba9c388cf
aydawka <aydan.gasimova2@gmail.com>
2023-06-07 09:40:34 -0700

    feat: ✨ added fetch functions for study and dataset

--- src/stores/services/service.ts
+++ src/stores/services/service.ts
@@ -1,11 +1,43 @@
-import type { Person } from "@/stores/publish/study-publish";
+import type { Participant, Study, ViewProfile } from "@/stores/publish/study-interfaces";
+import { Dataset, StudyVersion } from "@/stores/publish/study-interfaces";
+export async function fetchViewProfile(): Promise<ViewProfile[]> {
+  const response = await fetch(`http://localhost:5000/viewProfile}`);
+  return response.json();
+}
 
-export async function fetchParticipants(): Promise<Person[]> {
+export async function fetchParticipants(): Promise<Participant[]> {
   const response = await fetch("http://localhost:5000/participants");
   return response.json();
 }
 
-export async function fetchStudies(): Promise<any[]> {
-  const response = await fetch("http://localhost:5000/studies");
+export async function fetchStudies(): Promise<Study[]> {
+  const response = await fetch("http://localhost:5000/study");
   return response.json();
+}
+
+// today this has to be set up 1st TODO
+export async function fetchStudy(studyId: number): Promise<Study> {
+  const response = await fetch(`http://localhost:5000/study/${studyId}`);
+  return response.json();
+}
+
+export async function fetchDatasets(studyId: number): Promise<Dataset[]> {
+  const response = await fetch(`http://localhost:5000/study/${studyId}/dataset`);
+  return (await response.json()).map((d: Dataset) => Dataset.fromObject(d));
+}
+
+export async function fetchDataset(studyId: number): Promise<Dataset> {
+  const response = await fetch(`http://localhost:5000/study/${studyId}/dataset`);
+  return (await response.json()).map((d: Dataset) => Dataset.fromObject(d));
+}
+
+export async function fetchDatasetVersion(
+  studyId: number,
+  datasetId: number,
+  versionId: number
+): Promise<StudyVersion> {
+  const response = await fetch(
+    `http://localhost:5000/study/${studyId}/dataset/${datasetId}/version/${versionId}`
+  );
+  return StudyVersion.fromObject(await response.json());
 }
