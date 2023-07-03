b5873642b6b92b0506c2bfed61f84cb4400a77cb
aydawka <aydan.gasimova2@gmail.com>
2023-07-03 14:09:53 -0700

    fix: 🐛 localhost replaced with service

--- src/stores/services/service.ts
+++ src/stores/services/service.ts
@@ -1,28 +1,32 @@
 import type { Participant, Study, ViewProfile } from "@/stores/publish/study-interfaces";
 import { Dataset, DatasetVersion } from "@/stores/publish/study-interfaces";
 export async function fetchViewProfile(): Promise<ViewProfile[]> {
-  const response = await fetch(`http://localhost:5000/viewProfile}`);
+  const response = await fetch(`https://api-fairhub-io-staging.azurewebsites.net/viewProfile}`);
   return response.json();
 }
 
 export async function fetchParticipants(studyId: number): Promise<Participant[]> {
-  const response = await fetch(`http://localhost:5000/study/${studyId}/participants`);
+  const response = await fetch(
+    `https://api-fairhub-io-staging.azurewebsites.net/study/${studyId}/participants`
+  );
   return response.json();
 }
 
 export async function fetchStudies(): Promise<Study[]> {
-  const response = await fetch("http://localhost:5000/study");
+  const response = await fetch("https://api-fairhub-io-staging.azurewebsites.net/study");
   return response.json();
 }
 
 // today this has to be set up 1st TODO
 export async function fetchStudy(studyId: number): Promise<Study> {
-  const response = await fetch(`http://localhost:5000/study/${studyId}`);
+  const response = await fetch(`https://api-fairhub-io-staging.azurewebsites.net/study/${studyId}`);
   return response.json();
 }
 
 export async function fetchDatasets(studyId: number): Promise<Dataset[]> {
-  const response = await fetch(`http://localhost:5000/study/${studyId}/dataset`);
+  const response = await fetch(
+    `https://api-fairhub-io-staging.azurewebsites.net/study/${studyId}/dataset`
+  );
   return (await response.json()).map((d: Dataset) => Dataset.fromObject(d));
 }
 
@@ -37,7 +41,7 @@ export async function fetchDatasetVersion(
   versionId: number
 ): Promise<DatasetVersion> {
   const response = await fetch(
-    `http://localhost:5000/study/${studyId}/dataset/${datasetId}/version/${versionId}`
+    `https://api-fairhub-io-staging.azurewebsites.net/study/${studyId}/dataset/${datasetId}/version/${versionId}`
   );
   return DatasetVersion.fromObject(await response.json());
 }
