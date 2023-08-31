e4e802a460219b47411e85837c47a7633ddaa058
Sanjay Soundarajan <sanjay.16@live.com>
2023-08-31 10:59:27 -0700

    wip: 🚧 connect api

--- src/stores/study.ts
+++ src/stores/study.ts
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 import type { Study } from "@/types/Study";
+import { baseURL } from "@/utils/constants";
 
 export const useStudyStore = defineStore(
   "study",
@@ -29,35 +30,51 @@ export const useStudyStore = defineStore(
     const fetchAllStudies = async () => {
       loading.value = true;
 
-      // const response = await fetch(`${baseURL}/study`);
-      // allStudies.value = await response.json();
+      const response = await fetch(`${baseURL}/study`);
 
-      if (allStudies.value.length === 0) {
-        for (let i = 0; i < 5; i++) {
-          const s: Study = {
-            id: faker.string.uuid(),
-            title: faker.lorem.sentence(),
-            description: faker.lorem.paragraph(),
-            image: faker.image.urlPicsumPhotos(),
-            keywords: Array.from({ length: 3 }, () => faker.lorem.word()),
-            last_updated: faker.date.past().toISOString(),
-            owner: {
-              email: "sanjay@email.org",
-              first_name: faker.person.firstName(),
-              last_name: faker.person.lastName(),
-              orcid: faker.string.uuid(),
-            },
-            size: "4 GB",
-          };
-          allStudies.value.push(s);
-        }
+      // check for errors
+      if (!response.ok) {
+        throw new Error("Studies not found  ");
       }
+
+      allStudies.value = await response.json();
+
+      for (const study of allStudies.value) {
+        study.size = `${Math.round(Math.random() * 100)} MB`;
+        study.owner = {
+          email: "sanjay@email.org",
+          first_name: faker.person.firstName(),
+          last_name: faker.person.lastName(),
+          orcid: faker.string.uuid(),
+        };
+      }
+
+      // if (allStudies.value.length === 0) {
+      //   for (let i = 0; i < 5; i++) {
+      //     const s: Study = {
+      //       id: faker.string.uuid(),
+      //       title: faker.lorem.sentence(),
+      //       description: faker.lorem.paragraph(),
+      //       image: faker.image.urlPicsumPhotos(),
+      //       keywords: Array.from({ length: 3 }, () => faker.lorem.word()),
+      //       last_updated: faker.date.past().toISOString(),
+      //       owner: {
+      //         email: "sanjay@email.org",
+      //         first_name: faker.person.firstName(),
+      //         last_name: faker.person.lastName(),
+      //         orcid: faker.string.uuid(),
+      //       },
+      //       size: "4 GB",
+      //     };
+      //     allStudies.value.push(s);
+      //   }
+      // }
 
       console.log("studies", allStudies.value);
 
-      allStudies.value.forEach((study) => {
-        study.size = `${Math.round(Math.random() * 100)} MB`;
-      });
+      // allStudies.value.forEach((study) => {
+      //   study.size = `${Math.round(Math.random() * 100)} MB`;
+      // });
 
       /** Sort by name for now */
       allStudies.value.sort((a, b) => a.title.localeCompare(b.title));
@@ -67,25 +84,27 @@ export const useStudyStore = defineStore(
 
     const getStudy = async (studyId: string) => {
       loading.value = true;
-      /**
-       * TODO: this is temporary.
-       * TODO: We should be fetching the study from the server if it's not already fetched
-       */
 
-      // fetch all studies if not already fetched
-      if (allStudies.value.length === 0) {
-        await fetchAllStudies();
-      }
+      const response = await fetch(`${baseURL}/study/${studyId}`);
 
-      const s = allStudies.value.find((study) => study.id === studyId);
-
-      loading.value = false;
-
-      if (!s) {
+      // check for errors
+      if (!response.ok) {
         throw new Error("Study not found");
       }
 
+      const s = await response.json();
+
       study.value = s;
+
+      study.value.size = `${Math.round(Math.random() * 100)} MB`;
+      study.value.owner = {
+        email: "sanjay@email.org",
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        orcid: faker.string.uuid(),
+      };
+
+      loading.value = false;
 
       return study.value;
     };
