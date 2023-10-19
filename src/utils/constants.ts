143403766e29f5bf3d8c518b88814c4354a9697c
Sanjay Soundarajan <sanjay.16@live.com>
2023-10-19 13:52:20 -0700

    chore: 🔨 update URIs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import timezonesJSON from "@/assets/data/timezones.json";
 export const timezones = timezonesJSON;
 
 // export const baseURL = "https://dev.api.fairhub.io";
-export const baseURL = "http://localhost:5000";
+// export const baseURL = "http://localhost:5000";
 
-// export const baseURL =
-//   process.env.NODE_ENV === "production" ? "https://dev.api.fairhub.io" : "http://localhost:5000";
+export const baseURL =
+  process.env.NODE_ENV === "production" ? "https://dev.api.fairhub.io" : "http://localhost:5000";
