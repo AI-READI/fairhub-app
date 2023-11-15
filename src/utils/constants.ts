f0128ee64df7bd68210b25201c07afc8d2e6ac00
Sanjay Soundarajan <sanjay.16@live.com>
2023-11-14 18:15:14 -0800

    ci: 👷 add a staging deployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,4 @@ import timezonesJSON from "@/assets/data/timezones.json";
 
 export const timezones = timezonesJSON;
 
-// export const baseURL = "https://dev.api.fairhub.io";
-// export const baseURL = "http://localhost:5000";
-
-export const baseURL =
-  process.env.NODE_ENV === "production" ? "https://dev.api.fairhub.io" : "http://localhost:5000";
+export const baseURL = import.meta.env.VITE_FAIRHUB_API_URL || "http://localhost:5000";
