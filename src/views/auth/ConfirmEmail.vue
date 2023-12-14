e1641834ffb8c3407cecb4c70956670b44f7a41b
Sanjay Soundarajan <sanjay.16@live.com>
2023-12-14 12:27:23 -0800

    feat: ✨ add a sync indicator to confirm email

--- src/views/auth/ConfirmEmail.vue
+++ src/views/auth/ConfirmEmail.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import MailBoxAnimationJSON from "@/assets/animations/mailbox.json";
+import LottieLoader from "@/components/loader/LottieLoader.vue";
+import FadeTransition from "@/components/transitions/FadeTransition.vue";
 import { baseURL } from "@/utils/constants";
 
 const push = usePush();
@@ -7,11 +9,24 @@ const route = useRoute();
 const router = useRouter();
 
 const loading = ref(false);
+const emailVerifiedLoading = ref(false);
+
+const interval = ref(null);
 
 onBeforeMount(() => {
   if (!route.query.email) {
     router.push({ name: "auth:logout" });
   }
+
+  checkIfEmailIsVerified();
+
+  interval.value = setInterval(() => {
+    checkIfEmailIsVerified();
+  }, 3000);
+});
+
+onBeforeUnmount(() => {
+  clearInterval(interval.value);
 });
 
 const resendVerificationEmail = async () => {
@@ -46,10 +61,55 @@ const resendVerificationEmail = async () => {
     message: "Please check your email for a verification link.",
   });
 };
+
+const checkIfEmailIsVerified = async () => {
+  const email = route.query.email as string;
+
+  if (!email) {
+    return;
+  }
+
+  emailVerifiedLoading.value = true;
+
+  let response: Response;
+
+  try {
+    response = await fetch(`${baseURL}/auth/email-verification/check`, {
+      body: JSON.stringify({
+        email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+  } catch (error) {
+    emailVerifiedLoading.value = false;
+  }
+
+  emailVerifiedLoading.value = false;
+
+  if (!response.ok) {
+    push.error("Something went wrong. Please try again.");
+
+    throw new Error("Network response was not ok");
+  }
+
+  const { isVerified } = await response.json();
+
+  if (isVerified) {
+    push.success({
+      title: "Email verified successfully.",
+      message: "You can now log in.",
+    });
+
+    router.push({ name: "auth:login" });
+  }
+};
 </script>
 
 <template>
-  <main class="flex h-full w-full items-center justify-center">
+  <main class="relative flex h-full w-full items-center justify-center">
     <section class="mx-auto flex w-full max-w-screen-xl flex-col">
       <n-space vertical>
         <h1 class="pb-4 text-center text-5xl font-bold">Confirm your email address</h1>
@@ -84,5 +144,13 @@ const resendVerificationEmail = async () => {
         </n-button>
       </n-space>
     </section>
+
+    <FadeTransition>
+      <div class="absolute bottom-10 right-10 flex flex-col" v-if="emailVerifiedLoading">
+        <LottieLoader :width="100" :height="100" />
+
+        <span class="text-center font-medium"> Syncing... </span>
+      </div>
+    </FadeTransition>
   </main>
 </template>
