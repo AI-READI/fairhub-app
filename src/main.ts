1b29286c4ea23c150065930039db3742b8d8e987
slugb0t <wheresdorian@gmail.com>
2023-12-29 11:33:36 -0800

    refactor: ♻️ remove extra package and imports

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,9 @@ import "notivue/notifications.css"; // Only needed if using built-in notificatio
 import "notivue/animations.css"; // Only needed if using built-in animations
 import "./utils/fetchInterceptor";
 import "./utils/axiosInterceptor";
-// css for the markdown editor
-import "katex/dist/katex.min.css";
-import "@kangc/v-md-editor/lib/style/base-editor.css";
-import "@kangc/v-md-editor/lib/theme/style/github.css";
 
 // import "@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css";
 import { Icon } from "@iconify/vue";
-import VMdEditor from "@kangc/v-md-editor";
-import enUS from "@kangc/v-md-editor/lib/lang/en-US";
-import createAlignPlugin from "@kangc/v-md-editor/lib/plugins/align";
-import createKatexPlugin from "@kangc/v-md-editor/lib/plugins/katex/cdn";
-import createTodoListPlugin from "@kangc/v-md-editor/lib/plugins/todo-list/index";
-import githubTheme from "@kangc/v-md-editor/lib/theme/github.js";
 import { createFetch } from "@vueuse/core";
 import { createNotivue } from "notivue";
 import { createPinia } from "pinia";
@@ -28,12 +18,6 @@ import App from "./App.vue";
 import router from "./router";
 import { baseURL } from "./utils/constants";
 
-VMdEditor.use(githubTheme, {});
-VMdEditor.use(createKatexPlugin());
-VMdEditor.use(createTodoListPlugin());
-VMdEditor.use(createAlignPlugin());
-VMdEditor.lang.use("en-US", enUS);
-
 const app = createApp(App);
 
 const pinia = createPinia();
@@ -44,7 +28,6 @@ app.component("f-icon", Icon);
 app.use(pinia);
 app.use(router);
 app.use(Vue3Lottie, { name: "Vue3Lottie" });
-app.use(VMdEditor);
 
 // Make Axios available globally
 app.config.globalProperties.$axios = axios;
