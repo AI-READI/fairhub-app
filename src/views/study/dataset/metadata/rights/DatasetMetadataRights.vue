2b0cf7f39c26c4f2e2742b3ccfa6efe2150d1b6d
Dorian Portillo <50216901+slugb0t@users.noreply.github.com>
2024-01-02 03:13:37 -0800

    refactor: ♻️ dataset rights update (#33)
    * wip: 🚧 re-working rights page for license selection and editor
    
    * wip: 🚧 adding licenses to select picker
    
    * wip: 🚧 send license text to database after editing is finished
    
    * feat: ✨ license editor and selector created for dataset rights page
    
    * refactor: ♻️ apply editor to be the same as others
    
    * refactor: ♻️ remove extra package and imports

--- src/views/study/dataset/metadata/rights/DatasetMetadataRights.vue
+++ src/views/study/dataset/metadata/rights/DatasetMetadataRights.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
+import { config, MdEditor } from "md-editor-v3";
 import type { FormInst } from "naive-ui";
+import loading from "naive-ui/es/_internal/loading";
 import { nanoid } from "nanoid";
 
+import licensesJSON from "@/assets/data/licenses.json";
 import type { DatasetRights } from "@/types/Dataset";
 import { baseURL } from "@/utils/constants";
+import { sanitize } from "@/utils/helpers";
+import TargetBlankExtension from "@/utils/TargetBlankExtension";
+
+config({
+  editorConfig: {
+    languageUserDefined: {
+      "en-US": {
+        footer: {
+          markdownTotal: "Character Count",
+          scrollAuto: "Scroll Auto",
+        },
+      },
+    },
+  },
+  markdownItConfig(md) {
+    md.use(TargetBlankExtension);
+  },
+});
 
 const route = useRoute();
 const router = useRouter();
 const push = usePush();
+const displayLicenseEditor = ref(false);
+const draftLicense = ref("");
+const licenseName = ref("");
 
 const routeParams = {
   datasetId: route.params.datasetId as string,
@@ -39,11 +63,20 @@ onBeforeMount(async () => {
   console.log(data);
 
   moduleData.rights = data.map((item: any) => {
+    console.log(item);
     return {
       ...item,
       origin: "remote",
     };
   });
+  console.log(moduleData.rights.length);
+  if (moduleData.rights.length > 0) {
+    draftLicense.value = moduleData.rights[0].license_text;
+    console.log(moduleData.rights[0].rights);
+    licenseName.value = moduleData.rights[0].rights;
+    console.log(licenseName.value);
+    displayLicenseEditor.value = true;
+  }
 });
 
 const removeRight = async (id: string) => {
@@ -73,6 +106,7 @@ const addSubject = () => {
     id: nanoid(),
     identifier: "",
     identifier_scheme: "",
+    license_text: "",
     origin: "local",
     rights: "",
     uri: "",
@@ -86,7 +120,8 @@ const saveMetadata = (e: MouseEvent) => {
       const data: any = moduleData.rights.map((item) => {
         const entry = {
           identifier: item.identifier || "",
-          identifier_scheme: item.identifier_scheme || "",
+          identifier_scheme: draftLicense.value !== "" ? "SPDX" : "",
+          license_text: draftLicense.value || "",
           rights: item.rights,
           uri: item.uri || "",
         };
@@ -100,6 +135,7 @@ const saveMetadata = (e: MouseEvent) => {
           };
         }
       });
+      console.log(data);
 
       const response = await fetch(
         `${baseURL}/study/${studyId}/dataset/${datasetId}/metadata/rights`,
@@ -128,6 +164,60 @@ const saveMetadata = (e: MouseEvent) => {
     }
   });
 };
+
+const updateLicense = async (value: string) => {
+  console.log("updateLicense");
+  loading.value = true;
+  const license = licensesJSON.find((item) => item.name === value);
+
+  if (license) {
+    console.log(`${baseURL}/utils/requestjson?url=${encodeURIComponent(license.detailsUrl)}`);
+    const response = await fetch(
+      `${baseURL}/utils/requestjson?url=${encodeURIComponent(license.detailsUrl)}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      }
+    );
+
+    if (response.ok) {
+      const response_data = await response.json();
+      console.log(response_data.licenseText);
+      draftLicense.value = response_data.licenseText;
+      licenseName.value = license.name;
+      displayLicenseEditor.value = true;
+      loading.value = false;
+      if (moduleData.rights.length === 0) {
+        moduleData.rights.push({
+          id: nanoid(),
+          identifier: license.licenseId,
+          identifier_scheme: "SPDX",
+          license_text: draftLicense.value,
+          origin: "local",
+          rights: license.name,
+          uri: license.reference,
+        });
+      } else {
+        console.log(moduleData);
+        console.log(moduleData.rights[0]);
+        console.log(moduleData.rights[0].rights);
+        moduleData.rights[0] = {
+          ...moduleData.rights[0],
+          identifier: license.licenseId,
+          identifier_scheme: "SPDX",
+          rights: license.name,
+          uri: license.reference,
+        };
+      }
+      console.log(moduleData.rights);
+      console.log(license.name);
+    } else {
+      console.error("Failed to fetch license details:", response.status, response.statusText);
+    }
+  }
+};
 </script>
 
 <template>
@@ -142,7 +232,33 @@ const saveMetadata = (e: MouseEvent) => {
     <n-divider />
 
     <n-form ref="formRef" :model="moduleData" size="large" label-placement="top" class="pr-4">
-      <CollapsibleCard
+      <n-form-item label="Rights" path="rights">
+        <n-select
+          v-model="licenseName"
+          placeholder="MIT License Modern Variant."
+          clearable
+          :options="licensesJSON.map((option) => ({ label: option.name, value: option.name }))"
+          @update:value="updateLicense"
+          :value="licenseName !== '' ? licenseName : ''"
+        />
+      </n-form-item>
+
+      <div v-if="displayLicenseEditor" class="pb-5">
+        <MdEditor
+          v-model="draftLicense"
+          language="en-US"
+          preview-theme="github"
+          :show-code-row-number="true"
+          :sanitize="sanitize"
+        />
+        <!-- <v-md-editor
+          v-model="draftLicense"
+          height="800px"
+          left-toolbar="undo redo clear | h bold italic strikethrough | ul ol table hr | link"
+          right-toolbar="sync-scroll preview fullscreen"
+        ></v-md-editor> -->
+      </div>
+      <!-- <CollapsibleCard
         v-for="(item, index) in moduleData.rights"
         :key="item.id"
         class="mb-5 shadow-md"
@@ -204,7 +320,7 @@ const saveMetadata = (e: MouseEvent) => {
         </template>
 
         Add a new right
-      </n-button>
+      </n-button> -->
 
       <n-divider />
 
